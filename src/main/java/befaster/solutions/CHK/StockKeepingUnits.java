9aa6b239904f5f472006b57e60e8846be9f0a74b
sbx_user1051 <sbx_user1051@169.254.35.237>
2024-01-28 16:14:26 +0000

    Sun Jan 28 16:14:26 UTC 2024

--- src/main/java/befaster/solutions/CHK/StockKeepingUnits.java
+++ src/main/java/befaster/solutions/CHK/StockKeepingUnits.java
@@ -1,2 +1,20 @@
-package befaster.solutions.CHK;public enum StockKeepingUnits {
+package befaster.solutions.CHK;
+
+public enum StockKeepingUnits {
+    A(50),
+    B(30),
+    C(20),
+    D(15);
+
+    private final int price;
+
+    StockKeepingUnits(final int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
 }
+
