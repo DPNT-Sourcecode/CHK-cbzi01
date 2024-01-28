322a695e613eae124e41203883101b12ef12c814
sbx_user1051 <sbx_user1051@169.254.35.237>
2024-01-28 16:35:25 +0000

    Sun Jan 28 16:35:25 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,11 +2,39 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        for(char c : skus.toCharArray()) {
-            if(!StockKeepingUnits.values().) {
+        int totalSum = 0;
+        //List<Character> skusCount =
+
+        Map<Character, Integer> skusCount = groupAndCountNumberOfSameSkus(skus);
+
+        skusCount.forEach((
+
+        for (char sku : skus.toCharArray()) {
+            if (StockKeepingUnits.getStockKeepingPrice(sku) == -1) {
                 return -1;
             }
+
+            totalSum += StockKeepingUnits.getStockKeepingPrice(sku);
+        }
+
+    }
+
+    private Map<Character, Integer> groupAndCountNumberOfSameSkus(final String skus) {
+        Map<Character, Integer> skusCount = new HashMap<>();
+        for (char sku : skus.toCharArray()) {
+            if (skusCount.containsKey(sku)) {
+                skusCount.put(sku, skusCount.get(sku) + 1);
+            } else {
+                skusCount.put(sku, 1);
+            }
+        }
+        return skusCount;
     }
 }
+
--- src/main/java/befaster/solutions/CHK/StockKeepingUnits.java
+++ src/main/java/befaster/solutions/CHK/StockKeepingUnits.java
@@ -1,19 +1,34 @@
 package befaster.solutions.CHK;
 
 public enum StockKeepingUnits {
-    A(50),
-    B(30),
-    C(20),
-    D(15);
+    A('A', 50),
+    B('B', 30),
+    C('C', 20),
+    D('D', 15);
 
+    private final char sku;
     private final int price;
 
-    StockKeepingUnits(final int price) {
+    StockKeepingUnits(final char sku, final int price) {
+        this.sku = sku;
         this.price = price;
+    }
+
+    public char getSku() {
+        return sku;
     }
 
     public int getPrice() {
         return price;
     }
 
+    public static int getStockKeepingPrice(char sku) {
+        for (StockKeepingUnits stockKeepingUnit : StockKeepingUnits.values()) {
+            if(stockKeepingUnit.getSku() == sku) {
+                return stockKeepingUnit.getPrice();
+            }
+        }
+        return -1;
+    }
 }
+
