6f6b45f5e302c6caae61cddf660206a6d58098d9
sbx_user1051 <sbx_user1051@169.254.35.237>
2024-01-28 15:57:07 +0000

    Sun Jan 28 15:57:07 UTC 2024

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,16 +1,19 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
-
 @SuppressWarnings("unused")
 public class SumSolution {
 
     public int compute(int x, int y) {
-        
+        if (!validateNumberRangeBetweenZeroAndOneHundred(x) && !validateNumberRangeBetweenZeroAndOneHundred(y)) {
+            throw new IllegalArgumentException("Invalid number range");
+        }
+        return x + y;
+    }
 
-
-        throw new SolutionNotImplementedException();
+    private boolean validateNumberRangeBetweenZeroAndOneHundred(int number) {
+        return number >= 0 && number <= 100;
     }
 
 }
+
 
