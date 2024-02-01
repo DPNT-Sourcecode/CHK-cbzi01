5fcfb1575661291c3becbd0f0a68765e6bb04698
sbx_user1051 <sbx_user1051@169.254.48.77>
2024-02-01 13:49:05 +0000

    Thu Feb 01 13:49:05 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -199,7 +199,7 @@ class CheckoutSolutionTest {
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"K:70", "S:20", "X:17", "KK:120", "KKK:190", "KKKK:240"}, delimiter = ':')
+    @CsvSource(value = {"STX:45", "STXSTX:90", "SSSZ:65"}, delimiter = ':')
     void testForCHK_R5(String input, String expected) {
         CheckoutSolution checkoutSolution = new CheckoutSolution();
         int result = checkoutSolution.checkout(input);
@@ -207,3 +207,4 @@ class CheckoutSolutionTest {
     }
 
 }
+
